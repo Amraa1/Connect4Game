783a976ba04ce3affc97628a87bb8abb392d50c5
Amraa1 <amarsanaaganbaatar0409@gmail.com>
2024-10-04 14:01:58 +0800

    bug fix and emplementation of multi browser support

--- app.py
+++ app.py
@@ -2,18 +2,111 @@
 from connect4 import Connect4, PLAYER1, PLAYER2
 import asyncio
 import itertools
-from websockets.asyncio.server import serve
+import websockets
+from websockets.asyncio.server import broadcast, serve
 import json
+import secrets
 
 
-async def handler(websocket):
-    #initialize a Connect Four Game
+JOIN = {}
+
+async def start(websocket: websockets.WebSocketServerProtocol):
+    # Initialize a Connect Four Game, the set of Websockets connections
     game = Connect4()
+    connected = {websocket}
 
-    #Players take alternate turns
-    turns = itertools.cycle([PLAYER1, PLAYER2])
-    player = next(turns)
+    # receiving moves from this game, and secret access token
+    join_key = secrets.token_urlsafe(12)
+    JOIN[join_key] = game, connected
 
+    try:
+        # send secret access token to the browser of the player 1
+        # where it will be used for building a "join" link
+
+        event = {
+            "type": "init",
+            "join": join_key,
+        }
+        await websocket.send(json.dumps(event))
+
+
+        # Temporary - for testing
+        print("Player1 start the game", id(game))
+        # Send the first move, in case the first player already played it.
+        await replay(websocket, game)
+        await play(websocket, game, PLAYER1, connected)
+        
+    finally:
+        del JOIN[join_key]
+
+async def replay(websocket, game):
+    """
+    Send previous moves.
+
+    """
+    # Make a copy to avoid an exception if game.moves changes while iteration
+    # is in progress. If a move is played while replay is running, moves will
+    # be sent out of order but each move will be sent once and eventually the
+    # UI will be consistent.
+    for player, column, row in game.moves.copy():
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        await websocket.send(json.dumps(event))
+
+async def join(websocket, join_key):
+    event = {
+        "type": "player joined",
+        "player": PLAYER2,
+    }
+    await websocket.send(json.dumps(event))
+    # Find the connect4 game and get the set of connected websockets
+    try:
+        game, connected = JOIN[join_key]
+    except Exception as exc:
+        await error(websocket, f"Game not found: {exc}")
+
+    # Registered to receive moves from this game
+    connected.add(websocket)
+
+    try:
+        # Temporary for testing
+        print("Player2 join the game", id(game))
+        # Send the first move, in case the first player already played it.
+        await replay(websocket, game)
+        await play(websocket, game, PLAYER2, connected)
+    
+    finally:
+        connected.remove(websocket)
+
+async def error(websocket, message):
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+async def handler(websocket: websockets.WebSocketServerProtocol):
+    # Receive and parse tje "init" event from the UI
+    message = await websocket.recv()
+    event = json.loads(message)
+    assert event["type"] == "init"
+    
+    if "join" in event:
+        # Player 2 joins an existing game
+        join_key = event["join"]
+        await join(websocket, join_key)
+    
+    else:
+        # Player1 starts a new game
+        await start(websocket)
+
+
+
+async def play(websocket, game, player, connected):
     async for message in websocket:
         #Parse a "play" event from the UI
         event = json.loads(message)
@@ -24,32 +117,25 @@ async def handler(websocket):
             #Play the move
             row = game.play(player, column)
         except ValueError as exc:
-            event = {
-                "type": "error",
-                "message": str(exc),
-            }
-            await websocket.send(json.dumps(event))
-            continue
+            error_message = f"Invalid move: {exc}"
+            await error(websocket, error_message)
 
-    # Send a "play" event to update the UI.
+        # Send a "play" event to update the UI.
         event = {
             "type": "play",
             "player": player,
             "column": column,
             "row": row,
         }
-        await websocket.send(json.dumps(event))
+        broadcast(connected, json.dumps(event))
     
-    # If move is winning, send a "win" event.
-    if (game.winner == PLAYER1 or game.winner == PLAYER2):
-        event = {
-                "type": "win",
-                "player": game.winner,
-            }
-        await websocket.send(json.dumps(event))
-
-    # Alternate turns.
-    player = next(turns)
+        # If move is winning, send a "win" event.
+        if (game.winner == PLAYER1 or game.winner == PLAYER2):
+            event = {
+                    "type": "win",
+                    "player": game.winner,
+                }
+            broadcast(connected, json.dumps(event))
 
 
 async def main():
--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 import { createBoard, playMove } from "./connect4.js";
 
+const IPaddress = "197.163.56.124";
+
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+    // Send an "init" event according to who is connecting.
+    // get the URL parameters
+    const params = new URLSearchParams(window.location.search);
+    let event = { type: "init"};
+    if (params.has("join")) {
+      // Player 2 joins the game.
+      event.join = params.get("join");
+    } else {
+      // Player 1 creates the game.
+    }
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+// when the page loads -> someone connects to the server
 window.addEventListener("DOMContentLoaded", () => {
   // Initialize the UI.
   const board = document.querySelector(".board");
   createBoard(board);
+
+  // Websocket initialization
   // Open the WebSocket connection and register event handlers.
   const websocket = new WebSocket("ws://localhost:8001/");
+
+  initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
 });
@@ -32,8 +55,15 @@ function showMessage(message) {
 
 function receiveMoves(board, websocket) {
   websocket.addEventListener("message", ({ data }) => {
+    // turn JSON into js object type; JSON parse
     const event = JSON.parse(data);
     switch (event.type) {
+      case "init":
+        // Create a link for inviting Player2
+        const link = `${IPaddress}:8000/?join=${event.join}`;
+        document.querySelector(".join").value = `${link}`;
+        showMessage(`Send this link to Player2: ${link}`);
+        break;
       case "play":
         // Update the UI with the move.
         playMove(board, event.player, event.column, event.row);
@@ -46,8 +76,22 @@ function receiveMoves(board, websocket) {
       case "error":
         showMessage(event.message);
         break;
+      case "player joined":
+        document.querySelector(".join").classList.add("hidden");
+        break;
       default:
         throw new Error(`Unsupported event type: ${event.type}.`);
     }
   });
 }
+
+const linkBtn = document.querySelector(".join");
+linkBtn.addEventListener("click", () => {
+  console.log("Button clicked " + linkBtn);
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(linkBtn.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + linkBtn.value);
+});
